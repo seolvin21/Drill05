1f4e1caf91d3922df39f206b04282462eb8304dd
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-27 18:54:44 +0900

    linear movement practice

--- Lecture07_Linear_Movement/line.py
+++ Lecture07_Linear_Movement/line.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import math
 
 
 def stop():
@@ -42,7 +43,7 @@ def draw_big_point(p):
     turtle.goto(p)
     turtle.color(0.8, 0.9, 0)
     turtle.dot(15)
-    turtle.write('     '+str(p))
+    turtle.write('     ' + str(p))
 
 
 def draw_point(p):
@@ -51,14 +52,35 @@ def draw_point(p):
 
 
 def draw_line(p1, p2):
-    # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    x1, y1 = p1[0], p1[1]  # (-100, -100)
+    x2, y2 = p2[0], p2[1]  # (300, 150)
+
+    for i in range(0, 100, 5):
+        t = i / 100
+        x = (1 - t) * x1 + t * x2  # 1-t:t의 비율로 x1,x2를 섞고 더한다.
+        y = (1 - t) * y1 + t * y2
+
+        draw_point((x, y))
+
+    draw_point((x2, y2))  # 마지막 점을 확실히 찍기
 
 
 prepare_turtle_canvas()
 
+# points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
+points = [(random.randint(-300, 300), random.randint(-300, 300)) for i in range(10)]
 
-# fill here
+for p in points:
+    draw_big_point(p)
+
+for i in range(0, len(points) - 1):
+    draw_line(points[i], points[i + 1])
+draw_line(points[-1], points[0])
+# draw_line((-100, -100), (300, 150))
+# draw_line((-100, -100), (-100, 300))
 
 
 turtle.done()
